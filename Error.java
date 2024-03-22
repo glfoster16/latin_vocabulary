fa0d5cedb506ab463f9a56243c2a64818c6c78f0
gabrielfoster <glfoster@loyola.edu>
2024-03-22 01:20:09 -0400

    Addressed dealing with errors with the Microsoft Word functionality

--- Error.java
+++ Error.java
@@ -1,9 +1,19 @@
 public class Error extends Word{
 
     String searchedWord;
+    String latin;
+    String translation;
+    String partOfSpeech;
 
     public Error(String searchedWord){
         this.searchedWord = searchedWord;
+        this.latin = "Error";
+        this.translation = "Error";
+        this.partOfSpeech = "Error";
+
+        super.latin = "Error";
+        super.translation = "Error";
+        super.partOfSpeech = "Error";
     }
 
     public String toString(){
